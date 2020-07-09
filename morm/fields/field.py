eae26e864ff92a5293a26a033e6cbe68e0fdf5b0
Md Jahidul Hamid <jahidulhamid@yahoo.com>
2020-07-10 01:10:04 +0600

    update doc

--- morm/fields/field.py
+++ morm/fields/field.py
@@ -6,15 +6,32 @@ __copyright__ = 'Copyright © Md Jahidul Hamid <https://github.com/neurobin/>'
 __license__ = '[BSD](http://www.opensource.org/licenses/bsd-license.php)'
 __version__ = '0.0.1'
 
-
+from typing import Any, Optional
 from morm.types import Void
 
 
-def always_valid(value):
+def always_valid(value: Any)-> bool:
+    """Always return True regradless of the value.
+
+    Args:
+        value (Any): value to be validated.
+
+    Returns:
+        bool: True
+    """
     return True
 
-def nomodify(value):
+def nomodify(value: Any) -> Any:
+    """Return the value without any modification.
+
+    Args:
+        value (Any): Any value.
+
+    Returns:
+        Any: The save value that is passed.
+    """
     return value
+
 
 class Field(object):
     def __init__(self, sql_def, default=Void,
